2ab315caf4623bdde4675df43f32e6e7570d1842
Nikoloz Abuladze <nikaabuladze28@gmail.com>
2024-01-24 02:18:03 +0400

    FIRST UPLOAD JULY 19 2022

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('nav-links')[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("nav-links")[0];
 
- toggleButton.addEventListener('click', function(){
-    navbarLinks.classList.toggle('active')
-})
+toggleButton.addEventListener("click", function () {
+  navbarLinks.classList.toggle("active");
+});
+// FIRST UPLOAD JULY 19 2022
